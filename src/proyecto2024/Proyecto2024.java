9c53e8ab82ba644b169e206266de04044d41e940
facundo1987 <facundobustamante21@gmail.com>
2024-11-07 19:37:04 -0300

    agregue hola mundo

--- src/proyecto2024/Proyecto2024.java
+++ src/proyecto2024/Proyecto2024.java
@@ -5,6 +5,8 @@ import java.util.*;
 public class Proyecto2024 {
 
     public static void main(String[] args) {
+        
+        System.out.println("hola mundo");
 
         ArrayList<Pasajero> pasajeros = new ArrayList();
             System.out.println("Hola mundo");
